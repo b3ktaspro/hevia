d7c58b705ed0fd048eb90aed024cb6c702795975
b3ktas <b3ktas.pro@gmail.com>
2026-06-06 16:54:31 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,25 +5,18 @@ import Catalogue from "./Home/Catalogue";
 import Collections from "./Home/CollectionsSection";
 import Faq from "./Home/Faq";
 import Hero from "./Home/Hero";
-import Intro from "./Home/Intro";
-import Presentation from "./Home/Presentation";
-import Process from "./Home/Process";
 import Reviews from "./Home/Reviews";
-import Why from "./Home/Why";
+
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Collections />
-      <Intro />
       <BestSellers />
-      <Presentation />
       <Catalogue />
-      <Process />
-      <Faq />
-      <Why />
       <Advice />
+      <Faq />
       <Reviews />
       <Banner />
     </>
