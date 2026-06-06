acd6af8ced6c4686a1d851f553dc44c90fd14abd
b3ktas <b3ktas.pro@gmail.com>
2026-06-06 16:54:28 +0200

    Update Collections.jsx

--- src/pages/Collections.jsx
+++ src/pages/Collections.jsx
@@ -1,11 +1,21 @@
 import SurfacesSection from "./Collections/SurfacesSection";
 import CatalogueHighlight from "./Collections/CatalogueHighlight";
 
+import Intro from "./Home/Intro";
+
+import Presentation from "./Home/Presentation";
+import Process from "./Home/Process";
+import Why from "./Home/Why";
+
 export default function Collections() {
   return (
     <>
     <SurfacesSection />
     <CatalogueHighlight />
+    <Why />
+    <Process />
+    <Presentation />
+    <Intro />
     </>
   );
 }
