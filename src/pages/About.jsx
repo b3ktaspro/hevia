54584a391d1bbcd479fa41a81bf42bc35d50093a
b3ktas <b3ktas.pro@gmail.com>
2026-06-06 22:50:40 +0200

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,6 @@ import AboutSection from "./About/AboutSection";
 import DeliverySection from "./About/DeliverySection";
 import ExpertiseSection from "./About/ExpertiseSection";
 import ServicesSection from "./About/ServicesSection";
-import SkillsSection from "./About/SkillsSection";
 
 export default function About() {
   return (
@@ -10,7 +9,6 @@ export default function About() {
       <AboutSection />
       <DeliverySection />
       <ExpertiseSection />
-      <SkillsSection />
       <ServicesSection />
     </>
   );
